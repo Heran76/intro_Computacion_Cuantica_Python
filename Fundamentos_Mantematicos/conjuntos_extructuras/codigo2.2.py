89467aafaf07936c418ae58120455ad07ca5294b
Antonio Heredia Morante <heran76@gmail.com>
2026-02-15 12:20:52 +0100

    inicio operaciones con array

---
+++ Fundamentos_Mantematicos/conjuntos_extructuras/codigo2.2.py
@@ -0,0 +1,21 @@
+import numpy as np
+#Crea array x,y de 6 elementos y un valor escalar r
+x = np.linspace(0,1,6) #Primer array
+y = np.linspace(1, 2, 6)#segundo array
+r = 2 #validar escalar
+print(f'\n ******** Operaciones - funciones - con array ******************')
+print(f'Array x {x}')
+print(f'Array y {y}')
+print(f'Escalar r : {r}')
+
+#Operaciones aritméticas con array
+print(f'\n ******** Operaciones aritméticas ******************')
+print(f'x + y : {x+y}')
+print(f'x-y: {x-y}')
+print(f'x*y : {x*y}')
+print(f'x/y : {x/y}')
+
+#funciones sobre array
+print(f'\n ******** funciones sobre array ******************')
+print(f'sqrt(x): {np.sqrt(x)}')
+print(f'-x: {-x}')
