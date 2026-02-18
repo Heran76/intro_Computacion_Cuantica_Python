5c658981a5b5031f9cfe499168af396284fec160
Antonio Heredia Morante <heran76@gmail.com>
2026-02-18 18:26:24 +0100

    matplotlib2

--- Fundamentos_Mantematicos/Matpltlib/pyplot2d.py
+++ Fundamentos_Mantematicos/Matpltlib/pyplot2d.py
@@ -13,3 +13,18 @@ plt.figure()
 plt.plot([0,1,2],[2,3,4], 's', color='black')
 plt.plot([0,1,1,2],[3,2,4,3], 's', color='lightgray')
 plt.show()
+
+#Dibujar una circuferencia de radio r centrada en(0.0) con precisión de 
+#p puntos en el eje x
+r = 2
+p = 100
+x = np.linspace(-r, r, p)
+
+y_pos = np.sqrt(r**2 - x **2)
+y_neg = -np.sqrt(r**2 - x**2)  # Solo cambié esta línea (agregué el signo negativo)
+
+plt.figure(figsize=(5.5, 5.5))
+plt.plot(x, y_pos, color='black') #Puntos positivos
+plt.plot(x, y_neg, color='black') #Puntos negativos
+plt.axis('equal')  # Esta línea asegura que el círculo se vea redondo
+plt.show()
